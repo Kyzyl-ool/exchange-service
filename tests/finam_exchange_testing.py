f1ad2c7c031aaf66b325bc53606ea9b7fdad3209
Kyzyl-ool Kezhik <kyzyloolk@mail.ru>
2021-02-02 04:51:02 +0300

    more units

--- tests/finam_exchange_testing.py
+++ tests/finam_exchange_testing.py
@@ -7,14 +7,19 @@ class MyTestCase(unittest.TestCase):
     def setUp(self) -> None:
         self.exchange = FinamExchangeTestClient()
         self.amount_of_calls = 0
+        self.maximum = 0
 
     def test_handler_calls(self):
+        self.maximum = 0
+
         def handler(candle: Candle):
             self.amount_of_calls += 1
+            self.maximum = max(candle['close'], self.maximum)
 
         self.exchange.ohlc_subscribe('../data/TATN_210101_210131.csv', Timeframe.M1, handler)
 
         self.assertEqual(self.amount_of_calls, len(self.exchange.df))
+        self.assertAlmostEqual(self.maximum, self.exchange.df['<CLOSE>'].max(), delta=0.1)
 
 
 if __name__ == '__main__':
