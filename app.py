e5537355924ece62bdffaf55cb9f78145676d5c1
k.kyzylool <k.kyzylool@corp.mail.ru>
2021-03-29 14:40:55 +0300

    multithreading

--- app.py
+++ app.py
@@ -1,57 +1,94 @@
+from dotenv import load_dotenv
+
+import logging
+from datetime import datetime, timedelta
+
 from flask import Flask
-from flask_socketio import SocketIO, send, emit
-import configuration
-from flask import request
+from celery import Celery
+
+from victor.algorithm.momentum.complex import MainAlgorithm
+from victor.exchange.tinkoff import TinkoffExchangeClient
+from victor.exchange.types import Instrument, Candle, Timeframe
+from victor.generators.generator.filters.time_filter import Market
+from victor.risk_management.momentum import MomentumRiskManagement
+from victor.trader import Trader
+from threading import Thread
+
+import asyncio
+
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+broker_url = 'amqp://myuser:mypassword@localhost:5672/myvhost'
+celery_instance = Celery(app.name, broker=broker_url)
+celery_instance.conf.update(app.config)
+
+logging.basicConfig(level=logging.DEBUG)
 
 
-@socketio.on('subscribe')
-def handle_subscribe(data):
-    namespace = data['exchange'] + ':' + data['instrument']
-    configuration.exchanges[data['exchange']].ohlc_subscribe(
-        data['instrument'],
-        data['timeframe'],
-        lambda candle: send(candle, namespace=namespace)
-    )
+class Runner:
+    exchange = TinkoffExchangeClient()
+    loop = asyncio.get_event_loop()
+
+    def __init__(self):
+        self.instrument = Instrument(
+            punct=0.01,
+            id='BBG004730RP0'
+        )
+
+        self.risk_management = MomentumRiskManagement(
+            stop_loss=40,
+            d=100,
+            instrument=self.instrument,
+            alpha=0.2,
+            v0=10,
+        )
+        self.trader = Trader(
+            algorithms=[
+                MainAlgorithm(
+                    instrument=self.instrument,
+                    risk_management=self.risk_management,
+                    market=Market.rus
+                )
+            ],
+            exchange=self.exchange,
+            max_orders=1
+        )
+
+    def handler(self, candle: Candle):
+        self.trader.general_pool.update_generators(candle)
+        self.trader.perform_signals(candle)
+        logging.debug(candle)
+
+    async def init(self):
+        await self.exchange.init()
+
+    async def run(self):
+        await self.exchange.preload_candles(self.instrument, datetime.now() - timedelta(days=3, minutes=30),
+                                            datetime.now(),
+                                            Timeframe.M5)
+        await self.exchange.ohlc_subscribe('BBG004730RP0', Timeframe.M5, self.handler)
+
+    def run_sync(self):
+        self.loop.run_until_complete(self.init())
+        self.loop.run_until_complete(self.run())
 
 
-@app.route('/limit/<exchange_id>/<instrument>', methods=['POST'])
-def handle_limit_order(exchange_id, instrument):
-    punct = request.json['punct']
-    buy = request.json['buy']
-    price = request.json['price']
-    volume = request.json['volume']
-
-    return configuration.exchanges[exchange_id].limit_order({
-        'id': instrument,
-        'punct': punct,
-        'buy': buy,
-        'price': price,
-        'volume': volume
-    })
+runner = Runner()
 
 
-@app.route('/market/<exchange_id>/<instrument>', methods=['POST'])
-def handle_market_order(exchange_id, instrument):
-    punct = request.json['punct']
-    buy = request.json['buy']
-    volume = request.json['volume']
-
-    return configuration.exchanges[exchange_id].market_order({
-        'id': instrument,
-        'punct': punct,
-        'buy': buy,
-        'volume': volume
-    })
+def run():
+    runner.run_sync()
 
 
-@app.route('/orders/<exchange_id>/<instrument>', methods=['GET'])
-def handle_get_orders(exchange_id, instrument):
-    return configuration.exchanges[exchange_id].orders[instrument]
+def run_flask():
+    app.run()
 
 
 if __name__ == '__main__':
-    socketio.run(app, host=configuration.host, port=configuration.port)
+    thread = Thread(target=run)
+    thread.start()
+
+    thread2 = Thread(target=run_flask)
+    thread2.start()
+
